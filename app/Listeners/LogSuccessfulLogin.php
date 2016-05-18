9eba03210c307be5ee57c5911c3a53234cf18394
Max <pinchuk.maksim@gmail.com>
2016-05-18 18:15:25 +0300

    ...

---
+++ app/Listeners/LogSuccessfulLogin.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Listeners;
+
+use Carbon\Carbon;
+use Illuminate\Auth\Events\Login;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Support\Facades\Auth;
+
+class LogSuccessfulLogin
+{
+    /**
+     * Create the event listener.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Handle the event.
+     *
+     * @param  Login  $event
+     * @return void
+     */
+    public function handle(Login $event)
+    {
+        $user = Auth::user();
+        $user->last_login_at = Carbon::now();
+        $user->save();
+    }
+}
