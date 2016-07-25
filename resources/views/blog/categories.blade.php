079f0528c67c3a96534898aa772e54605ab8afb3
Yuri Kotlyar <atacante0666@gmail.com>
2016-07-25 17:13:23 +0300

    [*] User can see all blog categories https://www.pivotaltracker.com/story/show/126964177

--- resources/views/blog/categories.blade.php
+++ resources/views/blog/categories.blade.php
@@ -1,5 +1,8 @@
 <ul class="nav nav-pills nav-stacked nav-left community-menu" role="menu">
     @if($categories->count())
+         <li class="{{ !Request::input('category') ? 'active' : '' }}" role="presentation">
+             <a href="{{ url('blog') }}">All Categories</a>
+         </li>
         @foreach($categories as $category)
             <li class="{{ (Request::input('category') == $category->id) ? 'active' : '' }}" role="presentation">
                 <a href="{{ url('blog?category='.$category->id) }}"><i class="fa fa-btn fa-newspaper-o"></i>{{ $category->title }}</a>
