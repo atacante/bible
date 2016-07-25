3ec2fdd2c662f56afcfb2795c0fae4eb69146c07
Yuri Kotlyar <atacante0666@gmail.com>
2016-07-25 17:46:51 +0300

    [x] Admin not able to create category/article without required fields https://www.pivotaltracker.com/story/show/126960775

--- app/BlogArticle.php
+++ app/BlogArticle.php
@@ -5,6 +5,18 @@ class BlogArticle extends BaseModel {
 	protected $fillable = ['category_id', 'text', 'title', 'user_id'];
 	protected $dates = ['created_at', 'updated_at', 'published_at'];
 
+	public function rules()
+	{
+		$rules = [
+			'category_id' => 'required',
+			'title' => 'required|max:255',
+			'text' => 'required',
+
+		];
+
+		return $rules;
+	}
+
 	public function user() {
 		return $this->belongsTo(User::class, 'user_id', 'id');
 	}
--- app/BlogCategory.php
+++ app/BlogCategory.php
@@ -6,6 +6,15 @@ class BlogCategory extends BaseModel {
 
 	protected $fillable = ['title'];
 
+	public function rules()
+	{
+		$rules = [
+			'title' => 'required|max:255',
+		];
+
+		return $rules;
+	}
+
 	public function articles(){
 		return $this->hasMany(BlogArticle::class, 'category_id', 'id');
 	}
