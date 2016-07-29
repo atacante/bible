18a8a17c7bfedef2ada1f98a3b109ad895ad6b22
Yuri Kotlyar <atacante0666@gmail.com>
2016-07-29 19:31:42 +0300

    https://www.pivotaltracker.com/story/show/123200951 Created base structure of shop

---
+++ app/ShopProduct.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App;
+
+class ShopProduct extends BaseModel {
+
+	protected $fillable = ['category_id', 'name', 'photo', 'short_description', 'long_description', 'price', 'external_link'];
+	protected $dates = ['created_at', 'updated_at'];
+
+	public function rules()
+	{
+		$rules = [
+			'category_id' => 'required',
+			'name' => 'required|max:255',
+			'price' => 'required|numeric',
+		];
+
+		return $rules;
+	}
+
+	public function category() {
+		return $this->belongsTo(ShopCategory::class, 'category_id', 'id');
+	}
+
+}
