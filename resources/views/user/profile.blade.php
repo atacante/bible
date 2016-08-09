b224a58ce988ee7f721388ed09dfbe4dc969f50c
Max <pinchuk.maksim@gmail.com>
2016-08-09 18:40:51 +0300

    [x] User: Fixed misspelling

--- resources/views/user/profile.blade.php
+++ resources/views/user/profile.blade.php
@@ -166,7 +166,7 @@
 
                     </div>
                     <div class="form-group">
-                        {!! Form::label('subscribed', "Newslatter:") !!}
+                        {!! Form::label('subscribed', "Newsletter:") !!}
 
                         <div>
                             <label>
