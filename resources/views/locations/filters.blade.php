d1a72832347b3063dd975f8e4cd2b3af4c10a45d
Power <powerrrg@gmail.com>
2016-09-05 19:41:31 +0300

    [*] User can see location page with new design https://www.pivotaltracker.com/n/projects/1529199/stories/129701865

--- resources/views/locations/filters.blade.php
+++ resources/views/locations/filters.blade.php
@@ -3,8 +3,8 @@
 <div class="j-admin-verses-filters">
 {!! Form::open(['method' => 'get','url' => Request::url()]) !!}
 {{--{!! Form::select('version', array_merge(['all' => 'All Versions'],$filters['versions']), Request::input('version','all'),['class' => 'pull-left', 'style' => 'width: 245px; margin-right:10px;']) !!}--}}
-{!! Form::text('search',Request::input('search'),['placeholder' => 'Keyword...','class' => 'pull-left','style' => 'margin-right:10px; width:200px;']) !!}
+{!! Form::text('search',Request::input('search'),['placeholder' => 'Search for Locations...','class' => 'pull-left mr6']) !!}
 {!! Form::token() !!}
-{!! Form::button('Go',['type' => 'submit','class' => 'btn btn-primary pull-left']) !!}
+{!! Form::button('<i class="bs-search cu-search1"></i>',['type' => 'submit','class' => 'btn1 cu3-btn1 pull-left']) !!}
 {!! Form::close() !!}
 </div>
--- resources/views/locations/tabs.blade.php
+++ resources/views/locations/tabs.blade.php
@@ -1,14 +1,16 @@
 
     <div class="row">
-        <div class="col-md-12" style="margin-bottom: 30px">
-            <div class="location-people" style="height: 35px; border-bottom: 2px solid #cccccc;">
-                <div class="pull-left">
-                    {!! Html::link('/locations/list','LOCATIONS', ['class'=> (Request::is('locations/list'))?'active medium':'medium'] ) !!}
-                    {!! Html::link('/peoples/list','PEOPLE', ['class'=>(Request::is('peoples/list'))?'active medium':'medium']) !!}
-                </div>
-                <div class="pull-right">
+        <div class="col-md-12">
+            <ul class="nav nav-pills wall-nav" >
+                <li class="{{Request::is('locations/list')?'active':''}}">
+                    {!! Html::link('/locations/list','LOCATIONS') !!}
+                </li>
+                <li class="{{Request::is('peoples/list')?'active':''}}">
+                    {!! Html::link('/peoples/list','PEOPLE', ['class'=>(Request::is('peoples/list'))?'active':'']) !!}
+                </li>
+                <li class="pull-right">
                     @include('locations.filters')
-                </div>
-            </div>
+                </li>
+            </ul>
         </div>
     </div>
