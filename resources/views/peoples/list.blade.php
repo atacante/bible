7a4f81a5f137fcca3bfabc49069c439e55357cb6
Power <powerrrg@gmail.com>
2016-09-07 12:18:15 +0300

    [*] User can see people page with new design https://www.pivotaltracker.com/n/projects/1529199/stories/129916081

--- resources/views/peoples/list.blade.php
+++ resources/views/peoples/list.blade.php
@@ -8,63 +8,69 @@
     {{--    @include('reader.filters')--}}
     @include('locations.tabs')
     <div class="row">
-        <div class="col-md-12" style="line-height: 30px;">
+        <div class="col-md-12 locations-list" style="line-height: 30px;">
+            <div class="row cu1-row">
             @if(count($content['peoples']))
                 @foreach($content['peoples'] as $people)
-                    <div class="clearfix people-item">
-                        <h4>{!! $people->people_name !!}</h4>
-                        <div class="pull-left" style="margin-right: 10px;">
-                            @if($people->images->count())
-                                <div id="people-{!! $people->id !!}" class="carousel slide" data-ride="carousel" data-interval="{!! rand(5000,7000) !!}">
-                                    <!-- Indicators -->
-                                    @if($people->images->count() > 1)
-                                        <ol class="carousel-indicators">
-                                            @foreach($people->images as $key => $image)
-                                                <li data-target="#people-{!! $people->id !!}"
-                                                    data-slide-to="{!! $key !!}"
-                                                    class="{!! ($key == 0?'active':'') !!}"></li>
-                                            @endforeach
-                                        </ol>
-                                        @endif
+                    <div class="my-item">
+                        <div class="my-inner-location-item">
 
-                                                <!-- Wrapper for slides -->
-                                        <div class="carousel-inner" role="listbox">
-                                            @foreach($people->images as $key => $image)
-                                                <div class="item {!! ($key == 0?'active':'') !!} j-with-images">
-                                                    <img src="{!! Config::get('app.peopleImages').'thumbs/'.$image->image !!}"
-                                                         class="img-thumbnail" alt="" style="cursor: pointer;">
-                                                    {{--<div class="carousel-caption">--}}
-                                                    {{--</div>--}}
-                                                </div>
-                                            @endforeach
-                                        </div>
-
-                                        <!-- Controls -->
+                            <div class="">
+                                @if($people->images->count())
+                                    <div id="people-{!! $people->id !!}" class="carousel slide" data-ride="carousel" data-interval="{!! rand(5000,7000) !!}">
+                                        <!-- Indicators -->
                                         @if($people->images->count() > 1)
-                                            <a class="left carousel-control" href="#people-{!! $people->id !!}"
-                                               role="button" data-slide="prev">
-                                                <span class="glyphicon glyphicon-chevron-left"
-                                                      aria-hidden="true"></span>
-                                                <span class="sr-only">Previous</span>
-                                            </a>
-                                            <a class="right carousel-control" href="#people-{!! $people->id !!}"
-                                               role="button" data-slide="next">
-                                                <span class="glyphicon glyphicon-chevron-right"
-                                                      aria-hidden="true"></span>
-                                                <span class="sr-only">Next</span>
-                                            </a>
-                                        @endif
-                                </div>
-                            @endif
-                        </div>
-                        <div>
-                            {!! str_limit(strip_tags($people->people_description,'<p></p>'), $limit = 500, $end = '... '.Html::link(url('/peoples/view/'.$people->id,[],false), 'View Details', ['class' => 'btn btn-success','style' => 'padding: 0 5px;'], true)) !!}
+                                            <ol class="carousel-indicators">
+                                                @foreach($people->images as $key => $image)
+                                                    <li data-target="#people-{!! $people->id !!}"
+                                                        data-slide-to="{!! $key !!}"
+                                                        class="{!! ($key == 0?'active':'') !!}"></li>
+                                                @endforeach
+                                            </ol>
+                                            @endif
+
+                                                    <!-- Wrapper for slides -->
+                                            <div class="carousel-inner" role="listbox">
+                                                @foreach($people->images as $key => $image)
+                                                    <div class="item {!! ($key == 0?'active':'') !!} j-with-images">
+                                                        <img src="{!! Config::get('app.peopleImages').'thumbs/'.$image->image !!}"
+                                                             class="img-thumbnail" alt="" style="cursor: pointer;">
+                                                        {{--<div class="carousel-caption">--}}
+                                                        {{--</div>--}}
+                                                    </div>
+                                                @endforeach
+                                            </div>
+
+                                            <!-- Controls -->
+                                            @if($people->images->count() > 1)
+                                                <a class="left carousel-control" href="#people-{!! $people->id !!}"
+                                                   role="button" data-slide="prev">
+                                                    <span class="glyphicon glyphicon-chevron-left"
+                                                          aria-hidden="true"></span>
+                                                    <span class="sr-only">Previous</span>
+                                                </a>
+                                                <a class="right carousel-control" href="#people-{!! $people->id !!}"
+                                                   role="button" data-slide="next">
+                                                    <span class="glyphicon glyphicon-chevron-right"
+                                                          aria-hidden="true"></span>
+                                                    <span class="sr-only">Next</span>
+                                                </a>
+                                            @endif
+                                    </div>
+                                @endif
+                            </div>
+                            <div class="c-inner-location-text">
+                                <h4 class="h4-locations">{!! $people->people_name !!}</h4>
+                                {!! str_limit(strip_tags($people->people_description,'<p></p>'), $limit = 140, $end = '... '.Html::link(url('/peoples/view/'.$people->id,[],false), 'View Details', ['class' => 'btn1 cu2-btn1','style' => 'padding: 0 5px;'], true)) !!}
+                            </div>
+
                         </div>
                     </div>
                 @endforeach
             @else
                 <p class="text-center">No any results found</p>
             @endif
+            </div>
         </div>
     </div>
     <div class="row">
