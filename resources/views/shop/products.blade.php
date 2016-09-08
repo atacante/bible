ce128f7f5b1e991d0233214b7d5e759bbbcf53f1
Power <powerrrg@gmail.com>
2016-09-08 17:18:48 +0300

    [*] As a User, I want to see Shop Page with new design https://www.pivotaltracker.com/n/projects/1529199/stories/130001511

--- resources/views/shop/products.blade.php
+++ resources/views/shop/products.blade.php
@@ -1,37 +1,41 @@
 @if(count($products))
     @foreach($products as $product)
-        <div data-itemid="{!! $product->id !!}" class="col-md-6 clearfix product-item j-show-product" data-link="{!! ($product->external_link)? $product->external_link : url('/shop/product/'.$product->id,[],false) !!}" style="cursor: pointer">
-            <div class="pull-left" style="margin-right: 10px;">
-                @if($product->images->count())
-                    <img class="img-thumbnail" height="100" width="100" data-dz-thumbnail="" alt="" src="{!! Config::get('app.productImages').'thumbs/'.$product->images[0]->image !!}" />
-                @else
-                    <div class="no-avatar img-thumbnail">
-                        <div class="no-avatar-text text-center"><i class="fa fa-shopping-cart fa-4x"></i></div>
+        <div data-itemid="{!! $product->id !!}" class="my-item j-show-product" data-link="{!! ($product->external_link)? $product->external_link : url('/shop/product/'.$product->id,[],false) !!}" style="cursor: pointer">
+            <div class="my-inner-location-item">
+
+                <div class="pull-left" style="margin-right: 10px;">
+                    @if($product->images->count())
+                        <img class="img-thumbnail" height="100" width="100" data-dz-thumbnail="" alt="" src="{!! Config::get('app.productImages').'thumbs/'.$product->images[0]->image !!}" />
+                    @else
+                        <div class="no-avatar img-thumbnail">
+                            <div class="no-avatar-text text-center"><i class="fa fa-shopping-cart fa-4x"></i></div>
+                        </div>
+                    @endif
+                </div>
+                <div class="pull-left" style="margin-right: 10px; width: 200px;">
+                    <div><strong>{!! $product->name !!}</strong></div>
+                    <div style="line-height: 16px; font-size: 12px;">
+                        <span style="color:#90949c;">{!! $product->short_description !!}</span>
                     </div>
-                @endif
-            </div>
-            <div class="pull-left" style="margin-right: 10px; width: 200px;">
-                <div><strong>{!! $product->name !!}</strong></div>
-                <div style="line-height: 16px; font-size: 12px;">
-                    <span style="color:#90949c;">{!! $product->short_description !!}</span>
                 </div>
-            </div>
-            <div class="pull-right">
-                @if($product->external_link)
-                <div>
-                    {!! Html::link($product->external_link,'Show details', ['class'=>'btn btn-primary j-show-product', 'data-link' => $product->external_link]) !!}
-                </div>
-                <div style="margin-top: 10px;">
-                    ${!! $product->price !!}
-                </div>
-                @else
+                <div class="pull-right">
+                    @if($product->external_link)
                     <div>
-                        {!! Html::link(url('/shop/add-to-cart/'.$product->id,[],false),'Add To Cart', ['class'=>'btn btn-success']) !!}
+                        {!! Html::link($product->external_link,'Show details', ['class'=>'btn btn-primary j-show-product', 'data-link' => $product->external_link]) !!}
                     </div>
                     <div style="margin-top: 10px;">
                         ${!! $product->price !!}
                     </div>
-                @endif
+                    @else
+                        <div>
+                            {!! Html::link(url('/shop/add-to-cart/'.$product->id,[],false),'Add To Cart', ['class'=>'btn btn-success']) !!}
+                        </div>
+                        <div style="margin-top: 10px;">
+                            ${!! $product->price !!}
+                        </div>
+                    @endif
+                </div>
+
             </div>
         </div>
     @endforeach
