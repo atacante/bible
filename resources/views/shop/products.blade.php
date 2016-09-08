75cc1bb65b9cf0dd3967c475ba3f546eeba9105f
Power <powerrrg@gmail.com>
2016-09-08 18:39:17 +0300

    [*] As a User, I want to see Shop Page with new design https://www.pivotaltracker.com/n/projects/1529199/stories/130001511

--- resources/views/shop/products.blade.php
+++ resources/views/shop/products.blade.php
@@ -3,35 +3,32 @@
         <div data-itemid="{!! $product->id !!}" class="my-item j-show-product" data-link="{!! ($product->external_link)? $product->external_link : url('/shop/product/'.$product->id,[],false) !!}" style="cursor: pointer">
             <div class="my-inner-location-item">
 
-                <div class="pull-left" style="margin-right: 10px;">
-                    @if($product->images->count())
-                        <img class="img-thumbnail" height="100" width="100" data-dz-thumbnail="" alt="" src="{!! Config::get('app.productImages').'thumbs/'.$product->images[0]->image !!}" />
-                    @else
-                        <div class="no-avatar img-thumbnail">
-                            <div class="no-avatar-text text-center"><i class="fa fa-shopping-cart fa-4x"></i></div>
-                        </div>
-                    @endif
-                </div>
-                <div class="pull-left" style="margin-right: 10px; width: 200px;">
-                    <div><strong>{!! $product->name !!}</strong></div>
+
+                @if($product->images->count())
+                    {{--<img class="img-thumbnail" height="100" width="100" data-dz-thumbnail="" alt="" src="{!! Config::get('app.productImages').'thumbs/'.$product->images[0]->image !!}" />--}}
+                    <div class="product-image" data-dz-thumbnail="" data-image="{!!Config::get('app.productImages').'thumbs/'.$product->images[0]->image !!}" style="background: url('{!! $product->images[0]->image!=''?Config::get('app.productImages').'thumbs/'.$product->images[0]->image:'' !!}') center no-repeat;"></div>
+                @else
+                    <div class="product-image" data-dz-thumbnail="" data-image="" style="">
+                        <i class="bs-producticon cu-producticon"></i>
+                    </div>
+                @endif
+
+                <div class="c-inner-location-text">
+                    <h4 class="h4-locations">{!! $product->name !!}</h4>
                     <div style="line-height: 16px; font-size: 12px;">
                         <span style="color:#90949c;">{!! $product->short_description !!}</span>
                     </div>
-                </div>
-                <div class="pull-right">
-                    @if($product->external_link)
-                    <div>
-                        {!! Html::link($product->external_link,'Show details', ['class'=>'btn btn-primary j-show-product', 'data-link' => $product->external_link]) !!}
-                    </div>
-                    <div style="margin-top: 10px;">
+                    <div class="price-label">
                         ${!! $product->price !!}
                     </div>
+
+                    @if($product->external_link)
+                        <div>
+                            {!! Html::link($product->external_link,'Show details', ['class'=>'btn1 cu2-btn1 j-show-product', 'data-link' => $product->external_link]) !!}
+                        </div>
                     @else
                         <div>
-                            {!! Html::link(url('/shop/add-to-cart/'.$product->id,[],false),'Add To Cart', ['class'=>'btn btn-success']) !!}
-                        </div>
-                        <div style="margin-top: 10px;">
-                            ${!! $product->price !!}
+                            {!! Html::link(url('/shop/add-to-cart/'.$product->id,[],false),'Details', ['class'=>'btn2 cu2-btn1']) !!}
                         </div>
                     @endif
                 </div>
