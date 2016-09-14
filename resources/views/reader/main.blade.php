fdaeec11b212f11448373f84e0ed06471c1cf6eb
Yuri Kotlyar <atacante0666@gmail.com>
2016-09-14 18:05:00 +0300

    [x] Removed tags from shared prayer - https://www.pivotaltracker.com/story/show/114215733

--- resources/views/reader/main.blade.php
+++ resources/views/reader/main.blade.php
@@ -320,7 +320,7 @@
                                             {!! str_limit(strip_tags($item->text,'<p></p>'), $limit = 100, $end = '...') !!}
                                         </div>
                                         @if(Auth::user() && Auth::user()->isPremium() && $item->type == 'prayer')
-                                            <div class="addthis_sharing_toolbox" data-url="{{url('/community')}}" data-description="{!! $item->text !!}"></div>
+                                            <div class="addthis_sharing_toolbox" data-url="{{url('/community')}}" data-description="{!! strip_tags($item->text) !!}"></div>
                                         @endif
                                     </div>
                                 </div>
