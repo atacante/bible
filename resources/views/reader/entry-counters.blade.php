839c28987cca04536ef8a940320dcc0339b74edf
Power <powerrrg@gmail.com>
2016-09-20 17:24:45 +0300

    [*] User can see My Study Verse page with new design https://www.pivotaltracker.com/n/projects/1529199/stories/130724553

--- resources/views/reader/entry-counters.blade.php
+++ resources/views/reader/entry-counters.blade.php
@@ -1,4 +1,4 @@
-<div class="row">
+{{--<div class="row">
     <div class="col-lg-4 col-xs-6">
         <div class="mini-box" style="background-color: #f0ad4e">
             <div class="inner">
@@ -35,4 +35,30 @@
             <a href="{{ url('/prayers/create') }}" class="mini-box-footer j-create-prayer"> Create new Prayer <i class="fa fa-arrow-circle-right"></i> </a>
         </div>
     </div>
-</div>
+</div>--}}
+<ul class="study-second-panel">
+    <li>
+        <a href="{{ url('/notes/create') }}" class="j-create-note a-create-journey color7">
+            <div class="acj-counter">
+                <i class="bs-note"></i> {!! count($content['notes']) !!}
+            </div>
+            <div class="acj-title">Note{!! count($content['notes']) != 1?'s':'' !!}</div>
+        </a>
+    </li>
+    <li>
+        <a href="{{ url('/journal/create') }}" class="j-create-journal a-create-journey color6">
+            <div class="acj-counter">
+                <i class="bs-journal"></i> {!! count($content['journal']) !!}
+            </div>
+            <div class="acj-title">Journal Entr{!! count($content['journal']) != 1?'ies':'y' !!}</div>
+        </a>
+    </li>
+    <li>
+        <a href="{{ url('/prayers/create') }}" class="j-create-prayer a-create-journey color5">
+            <div class="acj-counter">
+                <i class="bs-pray"></i> {!! count($content['prayers']) !!}
+            </div>
+            <div class="acj-title">Prayer{!! count($content['prayers']) != 1?'s':'' !!}</div>
+        </a>
+    </li>
+</ul>
