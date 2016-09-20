3a723cad77ab7d269d98dca879beb575dc6a9c78
Power <powerrrg@gmail.com>
2016-09-21 01:31:57 +0300

    [*] User can see My Study Verse page with new design https://www.pivotaltracker.com/n/projects/1529199/stories/130724553

--- resources/views/reader/my-notes-table.blade.php
+++ resources/views/reader/my-notes-table.blade.php
@@ -1,57 +1,54 @@
 <div id="notes" class="verse-notes my-notes-list j-my-notes-list">
-    <h4 class="h4-study"><i class="bs-note cu-note"></i>My <span>Notes</span> for this Study Verse</h4>
-    {!! Html::link('/notes/create','Create Note', ['class'=>'btn1-kit j-create-note','style' => 'margin-bottom:10px;']) !!}
-    <table class="table table-bordered table-hover">
-        <thead>
-        <tr>
-            {{--<th width="20"><input type="checkbox" id="checkAll"/></th>--}}
-            <th>Note Text</th>
-            <th width="150">Tags</th>
-            <th width="100" class="text-center">Accessibility</th>
-            <th width="100" class="text-center">Created</th>
-            <th class="text-center" width="90">Actions</th>
-        </tr>
-        </thead>
-        <tbody>
+    <h4 class="h4-study mb3">
+        <i class="bs-note cu-note"></i>
+        My <span>Notes</span> for this Study Verse
+        {!! Html::link('/notes/create','Create Note', ['class'=>'btn1-kit cu-study-btn j-create-note']) !!}
+    </h4>
+
+    <ul class="study-list">
+
         @if($content['notes']->count())
             @foreach($content['notes'] as $note)
-                <tr>
-                    <td>
-                        <div class="note-text j-note-text"
-                             data-noteid="{!! $note->id !!}">{!! str_limit(strip_tags($note->note_text,'<p></p>'), $limit = 300, $end = '...') !!}</div>
-                    </td>
-                    <td>
-                        @if(count($note->tags))
-                            @foreach($note->tags as $tag)
-                                {{ Html::link(url('notes/list?'.http_build_query(['tags[]' => $tag->id]),[],false), $tag->tag_name, ['class' => 'label label-info'], true)}}
-                                <br/>
-                            @endforeach
-                        @endif
-                    </td>
-                    <td class="text-center">{!! ViewHelper::getAccessLevelIcon($note->access_level) !!}</td>
-                    <td class="text-center">{!! $note->created_at->format($note::DFORMAT) !!}</td>{{--H:i--}}
-                    <td class="text-center">
-                        <a title="Print note" href="#" data-noteid="{!! $note->id !!}" class="j-print-note"><i
-                                    class="fa fa-print fa-2x"
-                                    style="color: #367fa9; font-size: 1.4em; margin-right: 5px;"></i></a>
-                        <a title="Edit note" class="j-create-note"
-                           href="{!! url('/notes/update/'.$note->id) !!}"><i
-                                    class="fa fa-edit"
-                                    style="color: #367fa9; font-size: 1.4em; margin-right: 5px;"></i></a>
-                        <a title="Delete note" href="{!! url('/notes/delete',$note->id) !!}" data-toggle="modal"
-                           data-target="#confirm-delete" data-header="Delete Confirmation"
-                           data-confirm="Are you sure you want to delete this item?"><i class="fa fa-trash"
-                                                                                        style="color: #367fa9; font-size: 1.4em;"></i></a>
-                    </td>
-                </tr>
+                <li>
+
+                    <div class="note-text j-note-text" data-noteid="{!! $note->id !!}">
+                        {!! str_limit(strip_tags($note->note_text,'<p></p>'), $limit = 300, $end = '...') !!}
+                    </div>
+
+                    <div class="c-study-bottom">
+                        <div class="c-journey-tags">
+                            @if(count($note->tags))
+                                @foreach($note->tags as $tag)
+                                    {{ Html::link(url('notes/list?'.http_build_query(['tags[]' => $tag->id]),[],false), '#'.$tag->tag_name, ['class' => 'link-tag'], true)}}
+                                @endforeach
+                            @endif
+                        </div>
+                        <div class="pull-right">
+                            <a title="Print note" href="#" data-noteid="{!! $note->id !!}" class="j-print-note">
+                                <i class="fa fa-print fa-2x" style="color: #367fa9; font-size: 1.4em; margin-right: 5px;"></i>
+                            </a>
+                            <a title="Edit note" class="j-create-note" href="{!! url('/notes/update/'.$note->id) !!}">
+                                <i class="fa fa-edit" style="color: #367fa9; font-size: 1.4em; margin-right: 5px;"></i>
+                            </a>
+                            <a title="Delete note" href="{!! url('/notes/delete',$note->id) !!}" data-toggle="modal" data-target="#confirm-delete" data-header="Delete Confirmation" data-confirm="Are you sure you want to delete this item?">
+                                <i class="fa fa-trash"  style="color: #367fa9; font-size: 1.4em;"></i>
+                            </a>
+                        </div>
+                        <div class="c-journey-date cu-date1">
+                            {!! $note->humanLastUpdate($note::DFORMAT) !!}
+                            {{--{!! $note->created_at->format($note::DFORMAT) !!}--}}
+                            - {!! ViewHelper::getAccessLevelIcon($note->access_level) !!}
+                        </div>
+
+                    </div>
+
+                </li>
             @endforeach
         @else
-            <tr>
-                <td class="text-center" colspan="5">
+            <li class="text-center">
                     You haven’t got any notes yet.
-                </td>
-            </tr>
+            </li>
         @endif
-        </tbody>
-    </table>
+
+    </ul>
 </div>
