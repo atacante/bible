8f33b941447b4c38a43766d58c90489db553be98
Max <pinchuk.maksim@gmail.com>
2016-09-28 19:13:47 +0300

    [*] User: Implemented new design to "Report content" popup https://www.pivotaltracker.com/story/show/131247079

--- resources/views/community/report-form.blade.php
+++ resources/views/community/report-form.blade.php
@@ -1,8 +1,8 @@
-{!! Form::open(['method' => 'post', 'id' => 'report-form', 'class' => 'j-report-form','role' => 'form','files' => true]) !!}
+{!! Form::open(['method' => 'post', 'id' => 'report-form', 'class' => 'j-report-form entry-form','role' => 'form','files' => true]) !!}
 <div class="box-body">
     <div class="form-group {{ $errors->has('reason_text') ? ' has-error' : '' }}">
         {!! Form::label('reason_text', 'Reason:') !!}
-        {!! Form::textarea('reason_text',null,['id' => 'group-text','rows' => 3]) !!}
+        {!! Form::textarea('reason_text',null,['id' => 'group-text','rows' => 5]) !!}
         @if ($errors->has('reason_text'))
             <span class="help-block">
                 {{ $errors->first('reason_text') }}
@@ -12,8 +12,8 @@
 </div>
 <!-- /.box-body -->
 
-<div class="box-footer">
-    {!! Form::button('Send', ['type'=>'submit','class'=>'btn btn-primary j-send-report']) !!}
-    {!! Html::link('#','Cancel', ['class'=>'btn btn-danger','data-dismiss' => 'modal']) !!}
+<div class="box-footer" style="padding-top: 10px;">
+    {!! Form::button('Send', ['type'=>'submit','class'=>'btn2-kit cu-btn-pad1 j-send-report']) !!}
+    {!! Html::link('#','Cancel', ['class'=>'btn4-kit cu-btn-pad1','data-dismiss' => 'modal']) !!}
 </div>
 {!! Form::close() !!}
