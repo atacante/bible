833542959c8d2765a43ae544a6fbc82cc5110763
Power <powerrrg@gmail.com>
2016-09-28 19:50:18 +0300

    [*] User can see Blog Entry page with new design https://www.pivotaltracker.com/n/projects/1529199/stories/131277131

--- resources/views/blog/comments_form.blade.php
+++ resources/views/blog/comments_form.blade.php
@@ -1,19 +1,23 @@
 @if(Auth::check() && Auth::user()->isPremium())
-<div>
-    {!! Form::open(['method' => 'post','url' => '/blog/save-comment']) !!}
-    {!! Form::hidden('id',$article->id) !!}
-    <div class="form-group {{ $errors->has('text') ? ' has-error' : '' }}" style="margin: 0;">
-        {!! Form::textarea('text',Request::input('text'),['placeholder' => 'Write a comment...','class' => 'form-control','style' => '','rows' => 2]) !!}
-        @if ($errors->has('text'))
-            <span class="help-block">
-                {{ $errors->first('text') }}
-            </span>
-        @endif
+
+    <div class="c-wall-comment">
+        <div class="c-area-group2">
+            {!! Form::open(['method' => 'post','url' => '/blog/save-comment']) !!}
+            {!! Form::hidden('id',$article->id) !!}
+            <div class="form-group {{ $errors->has('text') ? ' has-error' : '' }}" style="margin: 0;">
+                {!! Form::textarea('text',Request::input('text'),['placeholder' => 'Write a comment...','class' => 'form-control','style' => '','rows' => 2]) !!}
+                @if ($errors->has('text'))
+                    <span class="help-block">
+                        {{ $errors->first('text') }}
+                    </span>
+                @endif
+            </div>
+            {!! Form::token() !!}
+            {!! Form::button('Post',['type' => 'submit','class' => 'btn4 btn4-cu1']) !!}
+            {!! Form::close() !!}
+        </div>
     </div>
-    {!! Form::token() !!}
-    {!! Form::button('Post',['type' => 'submit','class' => 'btn btn-primary','style' => 'padding: 2px 12px; margin-top:5px;']) !!}
-    {!! Form::close() !!}
-</div>
+
 @else
     <div style="margin: 10px 0;">Only premium users can leave comments</div>
 @endif
