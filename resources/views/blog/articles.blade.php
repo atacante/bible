54e6cfd8801c97ee81ac1a86c3c016bd9ce226dd
Power <powerrrg@gmail.com>
2016-09-28 17:02:28 +0300

    [x] User can see Blog page with new design https://www.pivotaltracker.com/n/projects/1529199/stories/131260337

--- resources/views/blog/articles.blade.php
+++ resources/views/blog/articles.blade.php
@@ -1,20 +1,22 @@
-@if($articles->count())
-    @foreach($articles as $article)
-        <div class="related-item">
-{{--            <div class="pull-left"><img src="{!! Config::get('app.blogImages').'thumbs/'.$article->img !!}"
-                               class="img-thumbnail" alt="" style="cursor: pointer;">
-            </div>--}}
-            <div class="clearfix">
-                <div class="item-header">{{$article->title}}<span class="pull-right">Posted by <b>{!! $article->user->name !!}</b> at <i>{!! $article->published_at->format($article::DFORMAT) !!}</i></span></div>
-                <div class="item-body j-show-article" data-link="{!! url('/blog/article/'.$article->id,[],false) !!}" >{!! str_limit($article->text, $limit = 800, $end = '... '.Html::link(url('/blog/article/'.$article->id,[],false), 'View Details', ['class' => 'btn btn-success','style' => 'padding: 0 5px;'], true)) !!}</div>
+<div class="c-white-content c-blog">
+    @if($articles->count())
+        @foreach($articles as $article)
+            <div class="related-item">
+                <div class="clearfix">
+                    <div class="item-header">
+                        <div class="cu-date1">&nbsp; {!! $article->humanLastUpdate() !!}</div>
+                        <h4 class="h4-sub-kit pull-left">{{$article->title}}</h4>
+                    </div>
+                    <div class="item-body j-show-article" data-link="{!! url('/blog/article/'.$article->id,[],false) !!}" >{!! str_limit($article->text, $limit = 800, $end = '... '.Html::link(url('/blog/article/'.$article->id,[],false), 'View Details', ['class' => 'btn btn-success','style' => 'padding: 0 5px;'], true)) !!}</div>
+                    <span class="pull-right">Posted by <b>{!! $article->user->name !!}</b></span>
+                </div>
             </div>
-        </div>
-    @endforeach
-@else
-    Nothing Found
-@endif
-
-<div class="row">
+        @endforeach
+    @else
+        Nothing Found
+    @endif
+</div>
+<div>
     <div class="text-center">
         {!! $articles->appends(Request::input())->links() !!}
     </div>
