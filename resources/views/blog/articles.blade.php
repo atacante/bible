7d3fcf3cbaa5d8d70daf2f50c247f3590173a6f5
Power <powerrrg@gmail.com>
2016-09-28 17:31:41 +0300

    [x] User can see Blog page with new design https://www.pivotaltracker.com/n/projects/1529199/stories/131260337

--- resources/views/blog/articles.blade.php
+++ resources/views/blog/articles.blade.php
@@ -3,11 +3,11 @@
         @foreach($articles as $article)
             <div class="related-item">
                 <div class="clearfix">
-                    <div class="item-header">
+                    <div class="item-header mb5">
                         <div class="cu-date1">&nbsp; {!! $article->humanLastUpdate() !!}</div>
                         <h4 class="h4-sub-kit pull-left">{{$article->title}}</h4>
                     </div>
-                    <div class="item-body j-show-article" data-link="{!! url('/blog/article/'.$article->id,[],false) !!}" >{!! str_limit($article->text, $limit = 800, $end = '... '.Html::link(url('/blog/article/'.$article->id,[],false), 'View Details', ['class' => 'btn btn-success','style' => 'padding: 0 5px;'], true)) !!}</div>
+                    <div class="item-body j-show-article wall-text1" data-link="{!! url('/blog/article/'.$article->id,[],false) !!}" >{!! str_limit($article->text, $limit = 800, $end = '... '.Html::link(url('/blog/article/'.$article->id,[],false), 'View Details', ['class' => 'btn btn-success','style' => 'padding: 0 5px;'], true)) !!}</div>
                     <span class="pull-right">Posted by <b>{!! $article->user->name !!}</b></span>
                 </div>
             </div>
