38d19fe71f38d5f7cd46d677a50d010fca46eccb
Power <powerrrg@gmail.com>
2016-09-29 14:45:47 +0300

    [*] User can see invate people page with new design https://www.pivotaltracker.com/n/projects/1529199/stories/131357099

--- resources/views/community/invite-people.blade.php
+++ resources/views/community/invite-people.blade.php
@@ -1,42 +1,54 @@
 @extends('layouts.app')
 
 @section('content')
-    {{--<h3 class="text-center">Find Friends</h3>--}}
     <div class="row friends-list j-friends-list">
-        <div class="col-md-2">
+        <div class="col-xs-3">
             @include('community.menu')
         </div>
-        <div class="col-md-10" style="">
-            {!! Form::open(['method' => 'post', 'id' => 'invite-form', 'class' => 'j-report-form','role' => 'form','files' => true]) !!}
-            <div class="form-group {{ $errors->has('emails') ? ' has-error' : '' }}">
-                {!! Form::label('emails', 'Emails of people to invite:') !!}
-                {!! Form::select('emails[]', [], [],['placeholder' => '','multiple' => true,'class' => 'clear-fix j-invite-emails', 'style' => '']) !!}
-                @if ($errors->has('emails'))
-                    <span class="help-block">
-                        {{ $errors->first('emails') }}
-                    </span>
-                @endif
-            </div>
-            <div class="form-group {{ $errors->has('invite_text') ? ' has-error' : '' }}">
-                {!! Form::label('invite_text', 'Invite text:') !!}
-                {!! Form::textarea('invite_text',$content['invite_text'],['placeholder' => '','multiple' => true,'class' => 'clear-fix ckeditor', 'style' => '']) !!}
-                @if ($errors->has('invite_text'))
-                    <span class="help-block">
-                        {{ $errors->first('invite_text') }}
-                    </span>
-                @endif
-                <div style="margin-top: 10px;">
-                    <strong>Placeholders:</strong><br />
-                    {invite_url} - {!! $content['invite_url'] !!}<br />
-                    {invite_link} - {!! $content['invite_link'] !!}
+        <div class="col-xs-9" style="">
+            <div class="c-white-content">
+                <div class="inner-pad2">
+
+                    {!! Form::open(['method' => 'post', 'id' => 'invite-form', 'class' => 'j-report-form','role' => 'form','files' => true]) !!}
+                    <div class="row form-group {{ $errors->has('emails') ? ' has-error' : '' }}">
+                        {!! Form::label('emails', 'Emails of people to invite', ["class"=>"col-xs-3 mt15"]) !!}
+                        <div class="col-xs-9">
+                            {!! Form::select('emails[]', [], [],['placeholder' => '','multiple' => true,'class' => 'clear-fix j-invite-emails', 'style' => 'max-width: 100%']) !!}
+                            @if ($errors->has('emails'))
+                                <span class="help-block">
+                                    {{ $errors->first('emails') }}
+                                </span>
+                            @endif
+                        </div>
+                    </div>
+                    <div class="row form-group {{ $errors->has('invite_text') ? ' has-error' : '' }}">
+                        {!! Form::label('invite_text', 'Invite text', ["class"=>"col-xs-3 mt15"]) !!}
+                        <div class="col-xs-9">
+                            {!! Form::textarea('invite_text',$content['invite_text'],['placeholder' => '','multiple' => true,'class' => 'clear-fix ckeditor', 'style' => '']) !!}
+                            @if ($errors->has('invite_text'))
+                                <span class="help-block">
+                                    {{ $errors->first('invite_text') }}
+                                </span>
+                            @endif
+                        </div>
+                    </div>
+                    <div class="row form-group">
+                        {!! Form::label('placeholders', 'Placeholders', ["class"=>"col-xs-3"]) !!}
+                        <div class="col-xs-9">
+                            {invite_url} - {!! $content['invite_url'] !!}<br />
+                            {invite_link} - {!! $content['invite_link'] !!}
+                        </div>
+                    </div>
+                    <div class="row form-group">
+                        <div class="col-xs-9 col-xs-offset-3">
+                            {!! Form::button('Send', ['type'=>'submit','class'=>'btn1-kit j-send-report']) !!}
+                        </div>
+                    </div>
+
+                    {!! Form::close() !!}
+
                 </div>
             </div>
-            <!-- /.box-body -->
-
-            <div class="box-footer">
-                {!! Form::button('Send', ['type'=>'submit','class'=>'btn btn-primary j-send-report']) !!}
-            </div>
-            {!! Form::close() !!}
         </div>
     </div>
 @endsection
