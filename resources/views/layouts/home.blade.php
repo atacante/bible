7202d3b2d373676e341fd91dc6247783cad21c62
Power <powerrrg@gmail.com>
2016-09-30 19:00:59 +0300

    [*] User can see responsive - mobile site  (Draft Version) https://www.pivotaltracker.com/n/projects/1529199/stories/131360311

--- resources/views/layouts/home.blade.php
+++ resources/views/layouts/home.blade.php
@@ -100,28 +100,11 @@
             }(document, 'script', 'facebook-jssdk'));
         </script>
 
-        <div class="container-fluid main-container">
-            <div class="inner-container">
-                <div class="bg-g1">
-                    <div class="in-inner-container text-center">
-                        @include('partials.nav-home')
-                        <h1 class="h1-1 mt1">ONLINE <span>STUDY BIBLE</span> COMMUNITY</h1>
-                        <h2 class="h2-3">Studying Scripture to Live a Praiseworthy Life to God</h2>
-                        <a href="{{ URL::to('/reader/read?version=nasb') }}" class="btn1 mt2 mb1">READ BIBLE NOW</a>
-                    </div>
-                </div>
 
-                    <div class="alert-container">
-                        @notification()
-                    </div>
 
-                        @yield('content')
+        @yield('content')
 
-                    @include('admin.partials.deletepop')
-                    @include('partials.popup')
 
-            </div>
-        </div>
 
         @include('partials.footer')
 
