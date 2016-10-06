34d38e34c181cc009e949b56d944f32ce3059f23
Max <pinchuk.maksim@gmail.com>
2016-10-06 15:27:31 +0300

    [x] Fixed search in People location https://www.pivotaltracker.com/story/show/131796843

--- resources/views/peoples/filters.blade.php
+++ resources/views/peoples/filters.blade.php
@@ -2,7 +2,7 @@
 ?>
 <div class="j-admin-verses-filters">
     {!! Form::open(['method' => 'get','url' => '/peoples/list']) !!}
-    {!! Form::text('search',Request::input('search'),['placeholder' => 'Keyword...','class' => 'search-text-input','style' => '']) !!}
+    {!! Form::text('search',Request::input('search'),['placeholder' => 'Search for People...','class' => 'search-text-input','style' => '']) !!}
     {{--{!! Form::select('book', array_merge([0 => 'All Books'],$filters['books']), Request::input('book'),['class' => 'pull-left', 'style' => 'width: 170px; margin-right:10px;']) !!}--}}
     {{--{!! Form::select('chapter',array_merge([0 => 'All Chapters'],(Request::input('book') == 0?[]:$filters['chapters'])), Request::input('chapter'),['class' => 'pull-left',(Request::input('book') == 0?'disabled':''), 'style' => 'width: 170px; margin-right:10px;']) !!}--}}
     {{--{!! Form::select('verse',array_merge([0 => 'All Verses'],(Request::input('chapter') == 0?[]:$filters['verses'])), Request::input('verse'),['class' => 'pull-left', (Request::input('chapter') == 0?'disabled':''), 'style' => 'width: 115px; margin-right:10px;']) !!}--}}
