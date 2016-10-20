52f4bbb10414d359398c5b6dc4473ed9bd7bae10
Yuri Kotlyar <atacante0666@gmail.com>
2016-10-20 17:44:25 +0300

    [*] Added Billing Address on signup - https://www.pivotaltracker.com/story/show/132758619

--- resources/views/user/profile.blade.php
+++ resources/views/user/profile.blade.php
@@ -117,6 +117,15 @@
                             </span>
                         @endif
                     </div>
+                    <div class="form-group">
+                        {!! Form::label('subscribed', "Newsletter:") !!}
+
+                        <div>
+                            {!! Form::hidden('subscribed', 0) !!}
+                            {!! Form::checkbox('subscribed', 1, $model->subscribed, ["class" => "cust-radio", "id" => "subscribed2"]) !!}
+                            <label for="subscribed2" class="label-checkbox cu-label  mt15">I want to get the latest news by my Email</label>
+                        </div>
+                    </div>
                 </div>
 
 
@@ -152,134 +161,127 @@
                             </span>
                         </div>
                     </div>
-                    <div class="form-group {{ $errors->has('plan_type') ? ' has-error' : '' }}">
-                        {!! Form::label('plan_type', "Subscription plan:") !!}
-
-                        <div>
-                            <div class="radio-inline mt15">
-                                {!! Form::radio('plan_type', 'free', true, ["class" => "cust-radio", 'id' => 'free']) !!}
-                                <label for="free" class="label-radio cu-label">Free</label>
-                            </div>
-                            <div class="radio-inline mt15">
-                                {!! Form::radio('plan_type', 'premium', false, ["class" => "cust-radio", 'id' => 'premium']) !!}
-                                <label for="premium" class="label-radio cu-label">Premium {!! $model->isPremiumPaid()?'(paid | Expires at: '.$model->getPlanExpiresAt().')' :'' !!}</label>
-                            </div>
-                            @if ($errors->has('plan_type'))
-                                <span class="help-block">
-                                        {{ $errors->first('plan_type') }}
-                                </span>
-                            @endif
-                        </div>
-                    </div>
-                    <div class="premium-only {!! $model->plan_type == 'free'?'hidden':'' !!}" >
-                        <div class="form-group {{ $errors->has('plan_name') ? ' has-error' : '' }}">
-                            {!! Form::label('plan_name', "Subscription plan period:") !!}
+                    <div class="billing-box">
+                        <div class="form-group {{ $errors->has('plan_type') ? ' has-error' : '' }}">
+                            {!! Form::label('plan_type', "Subscription plan:") !!}
 
                             <div>
-                                @foreach(App\User::getPossiblePlans() as $plan_name => $plan)
-                                <div class="radio-inline">
-                                    {!! Form::radio('plan_name', $plan_name, ($model->getActivePlan() == $plan_name), ["class" => "cust-radio", "id" => $plan_name]) !!}
-                                    <label class="label-radio cu-label" for="{{$plan_name}}"> {!! $plan_name.' ($'.$plan['amount'].')' !!}</label>
+                                <div class="radio-inline mt15">
+                                    {!! Form::radio('plan_type', 'free', true, ["class" => "cust-radio", 'id' => 'free']) !!}
+                                    <label for="free" class="label-radio cu-label">Free</label>
                                 </div>
-                                @endforeach
-                                @if ($errors->has('plan_name'))
+                                <div class="radio-inline mt15">
+                                    {!! Form::radio('plan_type', 'premium', false, ["class" => "cust-radio", 'id' => 'premium']) !!}
+                                    <label for="premium" class="label-radio cu-label">Premium {!! $model->isPremiumPaid()?'(paid | Expires at: '.$model->getPlanExpiresAt().')' :'' !!}</label>
+                                </div>
+                                @if ($errors->has('plan_type'))
                                     <span class="help-block">
+                                        {{ $errors->first('plan_type') }}
+                                </span>
+                                @endif
+                            </div>
+                        </div>
+                        <div class="premium-only {!! $model->plan_type == 'free'?'hidden':'' !!}" >
+                            <div class="form-group {{ $errors->has('plan_name') ? ' has-error' : '' }}">
+                                {!! Form::label('plan_name', "Subscription plan period:") !!}
+
+                                <div>
+                                    @foreach(App\User::getPossiblePlans() as $plan_name => $plan)
+                                        <div class="radio-inline">
+                                            {!! Form::radio('plan_name', $plan_name, ($model->getActivePlan() == $plan_name), ["class" => "cust-radio", "id" => $plan_name]) !!}
+                                            <label class="label-radio cu-label" for="{{$plan_name}}"> {!! $plan_name.' ($'.$plan['amount'].')' !!}</label>
+                                        </div>
+                                    @endforeach
+                                    @if ($errors->has('plan_name'))
+                                        <span class="help-block">
                                         {{ $errors->first('plan_name') }}
                                     </span>
-                                @endif
+                                    @endif
+                                </div>
                             </div>
-                        </div>
 
-                        <div class="form-group {{ $errors->has('coupon_code') ? ' has-error' : '' }}">
-                            {!! Form::label('coupon_code', 'Coupon Code:') !!}
-                            {!! Form::text('coupon_code', $model->coupon_code, ["class" => "input1"]) !!}
-                            @if ($errors->has('coupon_code'))
-                                <span class="help-block">
+                            <div class="form-group {{ $errors->has('coupon_code') ? ' has-error' : '' }}">
+                                {!! Form::label('coupon_code', 'Coupon Code:') !!}
+                                {!! Form::text('coupon_code', $model->coupon_code, ["class" => "input1"]) !!}
+                                @if ($errors->has('coupon_code'))
+                                    <span class="help-block">
                                     {{ $errors->first('coupon_code') }}
                                 </span>
-                            @endif
-                        </div>
-                    </div>
-                    <div class="row">
-                        <div class="col-xs-6">
-                            <div class="form-group">
-                                {!! Form::label('current_cc_number', 'Current CC number:') !!}
-                                <div class="{{$model->card_last_four!=''?'' : 'hide' }}">
-                                    <input class="form-control input1" type="text" value="**** **** **** {{$model->card_last_four}}" disabled="disabled">
-                                </div>
-                                <div class="{{$model->card_last_four!=''?'hide' : '' }}">
-                                    <input class="form-control input1" type="text" value="No Credit Card" disabled="disabled">
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col-xs-6">
-                            <div class="form-group {{ $errors->has('card_number') ? ' has-error' : '' }}">
-                                {!! Form::label('card_number', 'New CC Number:') !!}
-                                {!! Form::text('card_number', null, ['placeholder' => 'XXXXXXXXXXXXXX', 'class' => 'input1']) !!}
-                                @if ($errors->has('card_number'))
-                                    <span class="help-block">
-                                        {{ $errors->first('card_number') }}
-                                    </span>
                                 @endif
                             </div>
                         </div>
+                        <div class="row">
+                            <div class="col-xs-6">
+                                <div class="form-group">
+                                    {!! Form::label('current_cc_number', 'Current CC number:') !!}
+                                    <div class="{{$model->card_last_four!=''?'' : 'hide' }}">
+                                        <input class="form-control input1" type="text" value="**** **** **** {{$model->card_last_four}}" disabled="disabled">
+                                    </div>
+                                    <div class="{{$model->card_last_four!=''?'hide' : '' }}">
+                                        <input class="form-control input1" type="text" value="No Credit Card" disabled="disabled">
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-xs-6">
+                                <div class="form-group {{ $errors->has('card_number') ? ' has-error' : '' }}">
+                                    {!! Form::label('card_number', 'New CC Number:') !!}
+                                    {!! Form::text('card_number', null, ['placeholder' => 'XXXXXXXXXXXXXX', 'class' => 'input1']) !!}
+                                    @if ($errors->has('card_number'))
+                                        <span class="help-block">
+                                        {{ $errors->first('card_number') }}
+                                    </span>
+                                    @endif
+                                </div>
+                            </div>
 
-                    </div>
+                        </div>
 
-                    <div class="form-group {{ $errors->has('card_expiration') ? ' has-error' : '' }}">
-                        {!! Form::label('card_expiration', 'Credit Card Expiration:') !!}
-                        {!! Form::text('card_expiration', null, ['placeholder' => 'YYYY-MM', 'class' => 'input1', 'style' => 'width: 125px;']) !!}
-                        @if ($errors->has('card_expiration'))
-                            <span class="help-block">
+                        <div class="form-group {{ $errors->has('card_expiration') ? ' has-error' : '' }}">
+                            {!! Form::label('card_expiration', 'Credit Card Expiration:') !!}
+                            {!! Form::text('card_expiration', null, ['placeholder' => 'YYYY-MM', 'class' => 'input1', 'style' => 'width: 125px;']) !!}
+                            @if ($errors->has('card_expiration'))
+                                <span class="help-block">
                                 {{ $errors->first('card_expiration') }}
                             </span>
-                        @endif
-                    </div>
+                            @endif
+                        </div>
 
-                    <div class="form-group {{ $errors->has('card_code') ? ' has-error' : '' }}">
-                        {!! Form::label('card_code', 'Credit Card Code:') !!}
-                        {!! Form::text('card_code', null, ['placeholder' => '***', 'class' => 'input1', 'style' => 'width: 125px;']) !!}
-                        @if ($errors->has('card_code'))
-                            <span class="help-block">
+                        <div class="form-group {{ $errors->has('card_code') ? ' has-error' : '' }}">
+                            {!! Form::label('card_code', 'Credit Card Code:') !!}
+                            {!! Form::text('card_code', null, ['placeholder' => '***', 'class' => 'input1', 'style' => 'width: 125px;']) !!}
+                            @if ($errors->has('card_code'))
+                                <span class="help-block">
                                 {{ $errors->first('card_code') }}
                             </span>
-                        @endif
-                    </div>
+                            @endif
+                        </div>
 
-                    <div class="form-group {{ $errors->has('billing_name') ? ' has-error' : '' }}">
-                        {!! Form::label('billing_name', 'Billing Name:') !!}
-                        {!! Form::text('billing_name', ($model->userMeta)?$model->userMeta->billing_first_name .' '.$model->userMeta->billing_last_name: '', ['class' => 'input1']) !!}
-                        @if ($errors->has('billing_name'))
-                            <span class="help-block">
+                        <div class="form-group {{ $errors->has('billing_name') ? ' has-error' : '' }}">
+                            {!! Form::label('billing_name', 'Billing Name:') !!}
+                            {!! Form::text('billing_name', ($model->userMeta)?$model->userMeta->billing_first_name .' '.$model->userMeta->billing_last_name: '', ['class' => 'input1']) !!}
+                            @if ($errors->has('billing_name'))
+                                <span class="help-block">
                                     {{ $errors->first('billing_name') }}
                             </span>
-                        @endif
-                    </div>
-                    <div class="form-group {{ $errors->has('billing_address') ? ' has-error' : '' }}">
-                        {!! Form::label('billing_address', 'Billing Address:') !!}
-                        {!! Form::text('billing_address', ($model->userMeta)?$model->userMeta->billing_address:'', ['class' => 'input1']) !!}
-                        @if ($errors->has('billing_address'))
-                            <span class="help-block">
+                            @endif
+                        </div>
+                        <div class="form-group {{ $errors->has('billing_address') ? ' has-error' : '' }}">
+                            {!! Form::label('billing_address', 'Billing Address:') !!}
+                            {!! Form::text('billing_address', ($model->userMeta)?$model->userMeta->billing_address:'', ['class' => 'input1']) !!}
+                            @if ($errors->has('billing_address'))
+                                <span class="help-block">
                                     {{ $errors->first('billing_address') }}
                             </span>
-                        @endif
-                    </div>
-                    <div class="form-group {{ $errors->has('billing_zip') ? ' has-error' : '' }}">
-                        {!! Form::label('billing_zip', 'Billing Zip:') !!}
-                        {!! Form::text('billing_zip', ($model->userMeta)?$model->userMeta->billing_postcode:'', ['class' => 'input1']) !!}
-                        @if ($errors->has('billing_zip'))
-                            <span class="help-block">
+                            @endif
+                        </div>
+                        <div class="form-group {{ $errors->has('billing_zip') ? ' has-error' : '' }}">
+                            {!! Form::label('billing_zip', 'Billing Zip:') !!}
+                            {!! Form::text('billing_zip', ($model->userMeta)?$model->userMeta->billing_postcode:'', ['class' => 'input1']) !!}
+                            @if ($errors->has('billing_zip'))
+                                <span class="help-block">
                                     {{ $errors->first('billing_zip') }}
                             </span>
-                        @endif
-                    </div>
-                    <div class="form-group">
-                        {!! Form::label('subscribed', "Newsletter:") !!}
-
-                        <div>
-                            {!! Form::hidden('subscribed', 0) !!}
-                            {!! Form::checkbox('subscribed', 1, $model->subscribed, ["class" => "cust-radio", "id" => "subscribed2"]) !!}
-                            <label for="subscribed2" class="label-checkbox cu-label  mt15">I want to get the latest news by my Email</label>
+                            @endif
                         </div>
                     </div>
                 </div>
