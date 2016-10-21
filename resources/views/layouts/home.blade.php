7c3f31a6da901122224ee7a56401f9ab079ef1e3
Max <pinchuk.maksim@gmail.com>
2016-10-21 18:13:15 +0300

    [*] Implemented GA production ID https://www.pivotaltracker.com/story/show/132912595

--- resources/views/layouts/home.blade.php
+++ resources/views/layouts/home.blade.php
@@ -80,19 +80,7 @@
             e.async = 1, e.src = "https://inlinemanual.com/embed/player.1c7905f80a315c6bbe0bae5a64527dd9.js", e.charset = "UTF-8", t.parentNode.insertBefore(e, t)
         }();
     </script>
-    <script>
-        (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-                    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-                m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-        })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-
-        ga('create', 'UA-84561258-1', 'auto');
-        ga('create', 'UA-84986968-1', 'auto', {'name': 'betaTracker'});
-
-        ga('send', 'pageview');
-        ga('betaTracker.send', 'pageview');
-
-    </script>
+    @include('partials.ga')
     <div class="bg-home"
          style="background:
                  url('{{(!$homedata['home_main_block']->background)? '/images/bg-home-header.jpg':
