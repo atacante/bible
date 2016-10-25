e20b21773f784f6e1b7a4fbdcb83a52ef5f0778d
Yuri Kotlyar <atacante0666@gmail.com>
2016-10-25 14:36:02 +0300

    [!][x] Removed redirection on backURL in admin - https://www.pivotaltracker.com/story/show/133085977

--- resources/views/admin/shop/products/form.blade.php
+++ resources/views/admin/shop/products/form.blade.php
@@ -92,6 +92,6 @@
 
 <div class="box-footer">
     {!! Form::button('Save', ['type'=>'submit','class'=>'btn btn-primary']) !!}
-    {!! Html::link((($url = Session::get('backUrl'))?$url:'/admin/shop-products/list/'),'Cancel', ['class'=>'btn btn-default']) !!}
+    {!! Html::link('/admin/shop-products/list/','Cancel', ['class'=>'btn btn-default']) !!}
 </div>
 {!! Form::close() !!}
