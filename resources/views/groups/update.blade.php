419882b36be24147e20cc6787e2155c5b7f07950
Max <pinchuk.maksim@gmail.com>
2016-10-27 19:08:54 +0300

    [*] Improved design on create group page https://www.pivotaltracker.com/story/show/133270765

--- resources/views/groups/update.blade.php
+++ resources/views/groups/update.blade.php
@@ -5,13 +5,11 @@
         <div class="col-md-3">
             @include('community.menu')
         </div>
-        <div class="col-xs-9">
+        <div class="col-md-9">
             <div class="c-white-content mb2">
                 <div class="inner-pad2">
                     <h4 class="h4-kit" style="font-size: 18px; line-height: 22px"><i class="bs-edit cu-add"></i>Edit Group</h4>
-
                     @include('groups.form')
-
                 </div>
             </div>
         </div>
