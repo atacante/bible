8024fe2f87879de3bfff9d473465527a36e50433
Yuri Kotlyar <atacante0666@gmail.com>
2016-10-28 12:25:27 +0300

    [x] Show category when select article - https://www.pivotaltracker.com/story/show/133269059

--- resources/views/blog/categories.blade.php
+++ resources/views/blog/categories.blade.php
@@ -1,12 +1,12 @@
 <ul class="nav nav-pills nav-stacked nav-left community-menu c-white-content hide-menu j-hide-menu" role="menu">
     @if($categories->count())
-         <li class="{{ !Request::input('category') ? 'active' : '' }}" role="presentation">
+         <li class="{{ (!Request::input('category') && !isset($article)) ? 'active' : '' }}" role="presentation">
              <a href="{{ url('blog') }}">All Categories</a>
              <span class="caret"></span>
          </li>
         @foreach($categories as $key => $category)
             <li role="separator" class="divider {{($key > 9)?' hidden j-hidden':'' }}" style=""></li>
-            <li class="{{($key > 9)?'hidden j-hidden ':'' }}{{(Request::input('category') == $category->id) ? 'active' : '' }}" role="presentation">
+            <li class="{{($key > 9)?'hidden j-hidden ':'' }}{{((Request::input('category') == $category->id)||(isset($article)&&($article->category_id == $category->id))) ? 'active' : '' }}" role="presentation">
                 <a href="{{ url('blog?category='.$category->id) }}">{{ $category->title }}</a>
                 <span class="caret"></span>
             </li>
