90011706bd57f891927ab37ea74e2182b2b23212
Max <pinchuk.maksim@gmail.com>
2016-10-31 16:21:07 +0200

    [*] Admin: Improved verse of the day logic https://www.pivotaltracker.com/story/show/133107979

--- resources/views/admin/bible/verse_of_day.blade.php
+++ resources/views/admin/bible/verse_of_day.blade.php
@@ -6,7 +6,47 @@
 
 @section('content')
     <div class="box box-primary">
+        <div class="box-header" style="border-bottom: 1px solid #f4f4f4;">
+            <h4>Current verse:</h4>
+            <div class="row">
+                <div class="col-xs-3" style="width: 27.3%">
+                    @if($verseOfDay->image)
+                        <div class="pull-left" style="margin-right: 5px;">
+                            <img height="90" width="180" src="{!! Config::get('app.verseOfDayImages').'thumbs/'.$verseOfDay->image !!}" />
+                        </div>
+                    @endif
+                    @if($verseOfDay->image_mobile)
+                        <div class="pull-left">
+                            <img height="90" width="90" src="{!! Config::get('app.verseOfDayImages').'thumbs/'.$verseOfDay->image_mobile !!}" />
+                        </div>
+                    @endif
+                </div>
+                <div class="col-xs-9" style="width: 72.7%; padding-left: 0;">
+                    <div class="c-center-content2 mt8">
+                        <div class="col-left1 h-ill5">
+                            {{--<div class="luke">LUKE 6:37</div>--}}
+                        </div>
+                        <div class="col-right1">
+                            <p class="p-2">
+                                {!! $verseOfDay->verse->verse_text !!}
+                            </p>
+                            <span class="ital">
+                                <a href="{!! url('reader/verse?'.http_build_query(
+                                    [
+                                        'book' => $verseOfDay->verse->book_id,
+                                        'chapter' => $verseOfDay->verse->chapter_num,
+                                        'verse' => $verseOfDay->verse->verse_num
+                                    ]),[],false) !!}" class="" style="color: #7b828a;">
+                                    {!! strtoupper($verseOfDay->verse->booksListEn->book_name).' '.$verseOfDay->verse->chapter_num.':'.$verseOfDay->verse->verse_num !!}
+                                </a>
+                            </span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
         <div class="box-body j-admin-verses-filters">
+            <h4>Set new:</h4>
             {!! Form::open(['method' => 'post','url' => '/admin/bible/verseday','files' => true]) !!}
             <div class="form-group">
                 {!! Form::select('book', [0 => 'All Books']+$filters['books'], Request::input('book'),['class' => 'form-control', 'style' => 'width: 170px;']) !!}
@@ -35,13 +75,13 @@
             <div class="form-group">
                 {!! Form::label('image', 'Verse Image (desktop):') !!}
                 <div class="clearfix">
-                    <div id="img-thumb-preview" class="edit-images-thumbs product-images">
+                    {{--<div id="img-thumb-preview" class="edit-images-thumbs product-images">
                         @if($verseOfDay->image)
                             <div class="img-thumb">
                                 <img height="100" width="100" src="{!! Config::get('app.verseOfDayImages').'thumbs/'.$verseOfDay->image !!}" />
                             </div>
                         @endif
-                    </div>
+                    </div>--}}
                     <div class="fallback"> <!-- this is the fallback if JS isn't working -->
                         <input name="image" type="file" />
                     </div>
@@ -50,47 +90,23 @@
             <div class="form-group">
                 {!! Form::label('image', 'Verse Image (mobile):') !!}
                 <div class="clearfix">
-                    <div id="img-thumb-preview" class="edit-images-thumbs product-images">
+                    {{--<div id="img-thumb-preview" class="edit-images-thumbs product-images">
                         @if($verseOfDay->image_mobile)
                             <div class="img-thumb">
                                 <img height="100" width="100" src="{!! Config::get('app.verseOfDayImages').'thumbs/'.$verseOfDay->image_mobile !!}" />
                             </div>
                         @endif
-                    </div>
+                    </div>--}}
                     <div class="fallback"> <!-- this is the fallback if JS isn't working -->
                         <input name="image_mobile" type="file" />
                     </div>
                 </div>
             </div>
-            {!! Form::button('Set',['type' => 'submit','class' => 'btn btn-primary pull-left']) !!}
-            {!! Form::close() !!}
         </div>
-    </div>
-    <div class="row">
-        <div class="col-xs-12">
-            <div class="c-center-content2 mt8">
-                <div class="col-left1 h-ill5">
-                    {{--<div class="luke">LUKE 6:37</div>--}}
-                </div>
-                <div class="col-right1">
-                    <p class="p-2">
-                        {!! $verseOfDay->verse->verse_text !!}
-                    </p>
-                    <br />
-                    <p class="p-2">
-                        <span class="ital">
-                            <a href="{!! url('reader/verse?'.http_build_query(
-                                [
-                                    'book' => $verseOfDay->verse->book_id,
-                                    'chapter' => $verseOfDay->verse->chapter_num,
-                                    'verse' => $verseOfDay->verse->verse_num
-                                ]),[],false) !!}" class="" style="color: #7b828a;">
-                                {!! strtoupper($verseOfDay->verse->booksListEn->book_name).' '.$verseOfDay->verse->chapter_num.':'.$verseOfDay->verse->verse_num !!}
-                            </a>
-                        </span>
-                    </p>
-                </div>
-            </div>
+        <div class="box-footer">
+            {!! Form::button('Save',['type' => 'submit','class' => 'btn btn-primary pull-left']) !!}
+            {!! Html::link((($url = Session::get('backUrl'))?$url:'/admin/cms/list/'),'Cancel', ['class'=>'btn btn-default']) !!}
         </div>
+        {!! Form::close() !!}
     </div>
 @endsection
--- resources/views/admin/cms/list.blade.php
+++ resources/views/admin/cms/list.blade.php
@@ -13,7 +13,9 @@
                             <th>Content Type</th>
                             <th>System Name</th>
                             <th>Title</th>
+                            @if(Request::segment(3) != 'home')
                             <th class="text-center">Published</th>
+                            @endif
                             {{--<th>Content</th>--}}
                             {{--<th>Meta Title</th>--}}
                             {{--<th>Meta Keywords</th>--}}
@@ -26,6 +28,7 @@
                                     <td>{!! $page->content_type !!}</td>
                                     <td>{!! $page->system_name !!}</td>
                                     <td>{!! $page->title !!}</td>
+                                    @if(Request::segment(3) != 'home')
                                     <td class="text-center">
                                         @if($page->published)
                                             <span class="label label-success">yes</span>
@@ -33,6 +36,7 @@
                                             <span class="label label-danger">no</span>
                                         @endif
                                     </td>
+                                    @endif
                                     {{--<td>{!! $page->text !!}</td>--}}
                                     {{--<td>{!! $page->meta_title !!}</td>--}}
                                     {{--<td>{!! $page->meta_keywords !!}</td>--}}
@@ -42,6 +46,19 @@
                                     </td>
                                 </tr>
                             @endforeach
+                            @if(Request::segment(3) == 'home')
+                                <tr>
+                                    <td>home</td>
+                                    <td>verse_of_the_day</td>
+                                    <td>Verse of the day</td>
+                                    {{--<td class="text-center">
+                                        <span class="label label-success">yes</span>
+                                    </td>--}}
+                                    <td class="text-center" style="width: 50px;">
+                                        <a title="Edit CMS" href="{{ url('admin/bible/verseday') }}"><i class="fa fa-edit" style="color: #367fa9; font-size: 1.4em; margin-right: 5px;"></i></a>
+                                    </td>
+                                </tr>
+                            @endif
                         @else
                             <tr>
                                 <td colspan="9">
