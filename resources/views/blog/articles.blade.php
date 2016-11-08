525da3018c382b1ead8774ab8398831d25de7fb2
Yuri Kotlyar <atacante0666@gmail.com>
2016-11-08 14:56:41 +0200

    [*] Changed styles of blog titles - https://www.pivotaltracker.com/story/show/133849801

--- resources/views/blog/articles.blade.php
+++ resources/views/blog/articles.blade.php
@@ -5,7 +5,7 @@
                 <div class="clearfix">
                     <div class="item-header mb5">
                         <div class="cu-date1">&nbsp; {!! $article->humanLastUpdate() !!}</div>
-                        <h4 class="h4-sub-kit pull-left">{{$article->title}}</h4>
+                        <h2 class="h4-sub-kit pull-left j-show-article cursor-pointer" data-link="{!! url('/blog/article/'.$article->id,[]) !!}">{{ $article->title }}</h2>
                     </div>
                     <div class="item-body j-show-article wall-text1" data-link="{!! url('/blog/article/'.$article->id,[]) !!}" >{!! str_limit($article->text, $limit = 800, $end = '...<p>'.Html::link(url('/blog/article/'.$article->id,[]), 'View Details', ['class' => 'btn1-kit','style' => 'padding: 0 5px;'], true).'</p>') !!}</div>
                     <span class="pull-right">Posted by <b>{!! $article->author_name?$article->author_name:$article->user->name !!}</b></span>
