7ed26fdd2b1affa367406e6164d8e85807ff1806
Yuri Kotlyar <atacante0666@gmail.com>
2016-11-11 18:08:25 +0200

    [x] Isolate alpha from deploy scripts

--- bibleDABM.sh
+++ bibleDABM.sh
@@ -1,1 +1,2 @@
-git pull origin dev && git push origin dev && git checkout alpha && git pull origin alpha && git merge dev --no-edit && git push origin alpha && git checkout beta && git pull origin beta && git merge alpha --no-edit && git push origin beta && git checkout master && git pull origin master && git merge beta --no-edit && git push origin master && git checkout dev
+# && git checkout alpha && git pull origin alpha && git merge dev --no-edit && git push origin alpha
+git pull origin dev && git push origin dev && git checkout beta && git pull origin beta && git merge dev --no-edit && git push origin beta && git checkout master && git pull origin master && git merge beta --no-edit && git push origin master && git checkout dev
